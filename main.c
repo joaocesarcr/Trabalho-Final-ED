9739a3f8b4bcd20789b3c585a2f895ba4e12e2a6
Joao <joaocesarcriscolo@gmail.com>
2019-06-01 00:25:36 -0300

    First commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "funcoes.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[ ]) {
+  int cont;
+  
+  for(cont=0; cont < argc; cont++)
+    printf("%d Parametro: %s\n", cont,argv[cont]);
+  
+  return 0;
+}
